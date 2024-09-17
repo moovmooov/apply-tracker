5e14140e40baa0e91cdc8c5a3b99fc8c9e58638d
Mateus Vasconcelos <mateusmoov@gmail.com>
2024-09-17 18:37:24 -0300

    refactor(script): change the way to embeed the button

--- src/content.ts
+++ src/content.ts
@@ -3,12 +3,12 @@ import type { PlasmoCSConfig } from "plasmo";
 import { Storage } from "@plasmohq/storage";
 
 export const config: PlasmoCSConfig = {
-  matches: ["https://www.linkedin.com/jobs/collections/*"],
+  matches: ["https://www.linkedin.com/*"],
 };
 
 const BUTTON_CLASS =
-  "jobs-save-button artdeco-button artdeco-button--secondary artdeco-button--3 custom-button";
-const BUTTON_TEXT = "Add to Notion";
+  "jobs-save-button artdeco-button artdeco-button--secondary artdeco-button--3 applytracker-button";
+const BUTTON_TEXT = "Add to Notion Tracker";
 const BUTTON_LOADING_TEXT = "Adding...";
 const BUTTON_SUCCESS_TEXT = "Added!";
 const BUTTON_ERROR_TEXT = "Error adding";
@@ -36,12 +36,18 @@ async function createCustomButton() {
 }
 
 async function addCustomButton(container: Element) {
-  console.log("Attempting to add custom button to container");
-  if (!container.querySelector(".custom-button")) {
-    container.appendChild(await createCustomButton());
+  if (container.querySelector(".applytracker-button")) {
+    return;
+  }
+
+  try {
+    const button = await createCustomButton();
+    container.appendChild(button);
     console.log("Custom button added successfully");
-  } else {
-    console.log("Custom button already exists in container");
+  } catch (error) {
+    console.error("Error adding custom button:", error);
+    container.querySelector(".applytracker-button")?.remove();
+  } finally {
   }
 }
 
@@ -53,25 +59,19 @@ function setButtonState(
   if (!span) return;
 
   button.disabled = state === "loading";
+  span.textContent = {
+    default: BUTTON_TEXT,
+    loading: BUTTON_LOADING_TEXT,
+    success: BUTTON_SUCCESS_TEXT,
+    error: BUTTON_ERROR_TEXT,
+  }[state];
 
-  switch (state) {
-    case "default":
-      span.textContent = BUTTON_TEXT;
-      button.style.backgroundColor = "";
-      break;
-    case "loading":
-      span.textContent = BUTTON_LOADING_TEXT;
-      button.style.backgroundColor = "#f5f5f5";
-      break;
-    case "success":
-      span.textContent = BUTTON_SUCCESS_TEXT;
-      button.style.backgroundColor = "#d4edda";
-      break;
-    case "error":
-      span.textContent = BUTTON_ERROR_TEXT;
-      button.style.backgroundColor = "#f8d7da";
-      break;
-  }
+  button.style.backgroundColor = {
+    default: "",
+    loading: "#f5f5f5",
+    success: "#d4edda",
+    error: "#f8d7da",
+  }[state];
 }
 
 async function handleButtonClick(event: MouseEvent) {
@@ -88,8 +88,6 @@ async function handleButtonClick(event: MouseEvent) {
       ?.textContent?.trim() || "";
   const url = window.location.href.trim();
 
-  console.log(`Title: ${title}, Company: ${company}, URL: ${url}`);
-
   try {
     await sendToBackground({
       name: "embeed-button",
@@ -101,43 +99,40 @@ async function handleButtonClick(event: MouseEvent) {
     setButtonState(button, "error");
   }
 
-  // Reset button state after 2 seconds
   setTimeout(() => setButtonState(button, "default"), 2000);
 }
 
-function observeJobDetailsChanges() {
-  const observer = new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-      if (mutation.type === "childList") {
-        const jobDetailsCard = document.querySelector(
-          ".job-details-jobs-unified-top-card__job-title",
-        );
-        if (jobDetailsCard) {
-          handleButton();
-          observer.disconnect();
-          break;
-        }
-      }
-    }
-  });
-
-  observer.observe(document.body, { childList: true, subtree: true });
-}
-
-async function handleButton() {
+function handleJobPage() {
   const container = document
     .querySelector(".jobs-apply-button--top-card")
     ?.closest(".display-flex");
-
-  if (container && !container.querySelector(".custom-button")) {
-    await addCustomButton(container);
-    console.log("Custom button added after job details update");
+  if (container) {
+    addCustomButton(container);
   }
 }
 
-function initializeContentScript() {
-  console.log("Initializing content script");
-  observeJobDetailsChanges();
+function checkForURLChange() {
+  let lastURL = location.href;
+  const observer = new MutationObserver(() => {
+    const currentURL = location.href;
+    if (currentURL !== lastURL) {
+      lastURL = currentURL;
+      handleURLChange(currentURL);
+    }
+  });
+  observer.observe(document, { subtree: true, childList: true });
+
+  handleURLChange(lastURL);
 }
 
-window.addEventListener("load", initializeContentScript);
+function handleURLChange(url: string) {
+  if (url.includes("/jobs/")) {
+    setTimeout(() => {
+      handleJobPage();
+    }, 1000);
+  } else {
+    document.querySelector(".applytracker-button")?.remove();
+  }
+}
+
+checkForURLChange();
